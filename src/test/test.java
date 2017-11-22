f84d6614a0240191d9785318cd3f80ea5d64bc92
liuhouer <654714226@qq.com>
2017-11-22 15:28:15 +0800

    new post

--- src/test/test.java
+++ src/test/test.java
@@ -10,7 +10,7 @@ public class test {
 		//初始化连接
 		db.init();
 		//生成单个表
-		generateThing.generateOneModle("bc_soft", "d://opt//dir//dir111");
+		generateThing.generateOneModle("bc_tags", "d://opt//dir//dir111");
 	
 		//生成多个表
 	}
